b93e1a21edbec79a0d8f4da61bfb9360123d7ba4
syedalihussain100 <smuheminali@gmail.com>
2020-06-20 10:16:06 +0000

    Javascript Assignment

---
+++ App.js
@@ -0,0 +1,97 @@
+///////////////////  pdf 21-25//////////////////////
+// var user = prompt("Enter Yoour User Name");
+
+// var firstName = "Zaid";
+// var lastName = "Roy";
+// var fullName = firstName + " " + lastName;
+// alert(fullName);
+
+
+    // var data = "My Favourite Phone is: Samsung Galaxy S6 Eddge Plus";
+    // var check = data.length;
+    // document.write(check);
+
+    // var country = "Pakistani";
+    // var n = country.indexOf('n');
+    // alert(n);
+
+    // var z = "Hello World";
+    // var t = z.lastIndexOf('l');
+    // document.write(t);
+
+    // var e = "Pakistani";
+    // var x = e.indexOf('i');
+    // alert(x);
+
+    // var firstName = "Zaid";
+    // var lastName = "Roy";
+    // var z = firstName.concat(lastName);
+    // alert(z);
+
+// var f = "Hyderabad"
+
+//     var res = f.replace("Hyderabad", "Islamabad");
+// console.log(res);
+
+// var message = "Ali and Sami are best friends. They play cricket and football together";
+// console.log(message);
+
+// var value = "Value: " + 472 + "<br>";
+// var value1 = "Type: " + "String" + "<br/>";
+// var value2 = "Value: " + 472 + "<br/>";
+// var value3 = "Type: " + "Number" + '<br/>';
+// document.write(value,value1,value2,value3);
+
+// var caplital = "Users input: peanuts";
+// var search = caplital.toUpperCase();
+// console.log(search);
+
+// var g = "User input: Javascript" + "<br/>";
+// var w = "Title case: Javascript" + "<br/>";
+// document.write(g,w);
+
+// var num = "Number: " + 35.36  + "<br/>";
+// var result = "Result: " + 3536 + "<br/>";
+// document.write(num,result);
+
+
+
+// var p = prompt("Enter a Value");
+// switch (p) {
+//     case "@":
+//       alert("Note:\n ASCII code of @ is 64");
+//         break;
+//         case ".":
+//          alert("Note: \n ASCII code of . is 46");
+//          break;   
+//          case ",":
+//          alert("Note: \n ASCII code of , is 44");
+//          break;
+//          case "!":
+//           alert("Note: \n ASCII code of ! is 33"); 
+//           break;      
+//     default:
+//         alert("Note: \n ASCII ERROR");
+// }
+
+// var a = ["apple pie","cake","cookie","chips","patties"];
+// var t = prompt("Welcome to abc Cookie What do you want Sir/Mam");
+// switch (t) {
+//     case "cookie":
+//         alert("Cookie is avaible at index 2 in our bakery");        
+//         break;
+//         case "pastry":
+//         alert("We are sorry. pastry is not avaible in our bakery");
+//         break;
+//     default:
+//         alert("Note: \n Cookie is not avaible \n Error 403")
+// }
+
+// var university = "University of karachi";
+// var s = university.split("");
+// console.log(s);
+
+
+// var user =  "Pakistan";
+// var g = user.search('n');
+// console.log(g);
